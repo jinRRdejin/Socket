75e75f7979095fd0ca1559bcc18f94b4a0d2a0e4
jinruirui <237301924@qq.com>
2018-03-05 12:28:31 +0800

    解决server端显示不出收到信息的的问题

--- Socket/src/Message.java
+++ Socket/src/Message.java
@@ -1,33 +1,48 @@
-
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.Socket;
 
+
 public class Message implements Runnable{
-
-	Socket socket;
 	
+	Socket socket;
 
-	public Message(Socket socket) {
-		super();
+	public Message(Socket socket){
 		this.socket = socket;
 	}
 
 	public void run() {
+
 		InputStream in;
 		try {
 			in = socket.getInputStream();
-			 
-			byte[] bytes = new byte[in.available()];
+			
+			int len = in.available();
+			
+			while(len == 0){
+				
+				Thread.sleep(100);
+				len = in.available();
+			}
+			byte[] bytes = new byte[len];
 			in.read(bytes);
 			System.out.println(new String(bytes,"utf-8"));
 			in.close();
-		} catch (IOException  e) {
+			
+			
+			
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}	
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+		
+		
 		
 	}
 
+	
 }
